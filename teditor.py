db48ce1d6e0306acf16993e9bd128a32307387f8
root <sujanmondal916@gmail.com>
2019-12-05 00:31:32 +0530

    text area and scrollbar

--- teditor.py
+++ teditor.py
@@ -1,13 +1,31 @@
 from tkinter import *
 
 
-class teditor:
+class Notepad:
+    __root = Tk()
 
-    def __init__(self, root):
-        root.title("text editor")
+    # default window width and height
+    __thisWidth = 300
+    __thisHeight = 300
+    __thisTextArea = Text(__root)
+    __thisScrollBar = Scrollbar(__thisTextArea)
+
+    def __init__(self, **kwargs):
+        #text area and scrollbar
+        self.__thisWidth = kwargs['width']
+        self.__thisHeight = kwargs['height']
+        self.__root.title("notepad")
+        self.__root.geometry('%dx%d' % (self.__thisWidth, self.__thisHeight))
+        self.__root.grid_rowconfigure(0, weight=1)
+        self.__root.grid_columnconfigure(0, weight=1)
+        self.__thisTextArea.grid(sticky=N + E + S + W)
+        self.__thisScrollBar.pack(side=RIGHT, fill=Y)
+        self.__thisScrollBar.config(command=self.__thisTextArea.yview)
+        self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
+
+    def run(self):
+        self.__root.mainloop()
 
 
-r = Tk()
-r.geometry("200x300")
-TE = teditor(r)
-r.mainloop()
+notepad = Notepad(width=600, height=600)
+notepad.run()
